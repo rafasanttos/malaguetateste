f2987f748d276c39b2a5f5670d19dc833ce4d3dc
RAFAEL DOS SANTOS <79388000+rafasanttos@users.noreply.github.com>
2024-04-29 17:04:38 -0300

    subir arquivos do site
    ainda não finalizado

---
+++ script.js
@@ -0,0 +1,9 @@
+// let contador = 1;
+
+// setInterval(function(){
+//     document.getElementById('radio'+contador).checked = true;
+//     contador++;
+//     if(contador>6){
+//         contador=1;
+//     }
+// },3000);
